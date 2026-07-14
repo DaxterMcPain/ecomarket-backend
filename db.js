e902e97f45a657c7d43c4d09c8eef9de7c3770d1
DaxterMcPain <yamil-aldana-izquierdo@hotmail.com>
2026-07-13 23:25:46 -0500

    feat: configurar variables de entorno y soporte SSL en db.js

--- db.js
+++ db.js
@@ -1,21 +1,18 @@
 import mysql from "mysql2/promise";
+import 'dotenv/config';
 
 const db = mysql.createPool({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "contra123",
+    database: process.env.DB_DATABASE || "ecomarket",
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
 
-    host: "localhost",
-
-    user: "root",
-
-    password: "contra123",
-
-    database: "ecomarket",
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : null,
 
     waitForConnections: true,
-
     connectionLimit: 10,
-
     queueLimit: 0
-
 });
 
 export default db;
